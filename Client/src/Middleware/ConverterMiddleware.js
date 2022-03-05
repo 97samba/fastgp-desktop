931b80e8e92bd074c42709679bebbd5f50bcef7e
Samba NDIAYE <97samba@gmail.com>
2022-03-05 13:31:10 +0100

    near stable transit

---
+++ Client/src/Middleware/ConverterMiddleware.js
@@ -0,0 +1,18 @@
+const currencies = {
+    EUR: {
+        USD: 1.1,
+        GBP: 0.83,
+        XOF: 655,
+        MAD: 10.65,
+        CAD: 1.4,
+    },
+    USD: {
+        EUR: 0.91,
+        GBP: 0.75,
+        XOF: 595,
+        MAD: 9.68,
+        CAD: 1.27,
+    },
+};
+
+export function convert(from, to) {}
